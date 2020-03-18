c9a208f3a0c0c03891d19285ef9aa0481c6cecff
최석준 <@DESKTOP-LVBSNLU>
2020-03-18 23:24:00 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		 System.out.println("*** Subject Management System Menu***");
+		 System.out.println(" 1. Add Subject");
+		 System.out.println(" 2. Delete Subject");
+		 System.out.println(" 3. Edit Subject");
+		 System.out.println(" 4. View Subject");
+		 System.out.println(" 5. Exit");
+		 System.out.println("Select one number between 1 - 5");
+	}
+
+}
