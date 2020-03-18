2b235123de8d54129f7fbc8e7e7dd41d46eb0f15
최석준 <@DESKTOP-LVBSNLU>
2020-03-18 23:38:27 +0900

    rename the file name from hello world to menu manager

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,6 +1,6 @@
-public class HelloWorld {
+public class MenuManager {
 	public static void main(String[] args) {
-		 System.out.println("*** Subject Management System Menu***");
+		 System.out.println("*** Subject Management System Menu ***");
 		 System.out.println(" 1. Add Subject");
 		 System.out.println(" 2. Delete Subject");
 		 System.out.println(" 3. Edit Subject");
