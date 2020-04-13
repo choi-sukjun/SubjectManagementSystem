8a87fe3dda66de53ced4c2ba29a98c2dd749a9ef
최석준 <@DESKTOP-LVBSNLU>
2020-04-13 16:50:33 +0900

    Multiple subjects entered

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -11,7 +11,7 @@ public class MenuManager {
 			 System.out.println(" 1. Add Subject");
 			 System.out.println(" 2. Delete Subject");
 			 System.out.println(" 3. Edit Subject");
-			 System.out.println(" 4. View Subject");
+			 System.out.println(" 4. View Subjects");
 			 System.out.println(" 5. Exit");
 			 System.out.println("Select one number between 1 - 5:");
 			 num = input.nextInt();
@@ -25,7 +25,7 @@ public class MenuManager {
 				 subjectManager.editSubject();
 			 }
 			 else if (num == 4) {
-				 subjectManager.viewSubject();
+				 subjectManager.viewSubjects();
 			 }
 			 else {
 				 continue;
--- src/SubjectManager.java
+++ src/SubjectManager.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class SubjectManager {
-	Subject subject;
+	ArrayList<Subject> subjects = new ArrayList<Subject>();
 	Scanner input;
 	SubjectManager(Scanner input) {
 		this.input = input;
 	}
 	
 	public void addSubject() {
-		 subject = new Subject();
+		 Subject subject = new Subject();
 		 System.out.print("Subject ID: ");
 		 subject.id= input.nextInt();
 		 System.out.print("Subject name: ");
@@ -22,69 +24,84 @@ public class SubjectManager {
 		 subject.finishtime = input.nextInt();
 		 System.out.print("Professor: ");
 		 subject.professor = input.next();
+		 subjects.add(subject);
 	}
 	
 	public void deleteSubject() {
 		 System.out.print("Subject ID: ");
 		 int subjectId = input.nextInt();
-		 if (subject == null) {
+		 int index = -1;
+		 for (int i = 0; i<subjects.size(); i++) {
+			 if (subjects.get(i).id == subjectId) {
+				 index = i;
+				 break;
+			 }
+		 }
+		 
+		 if (index >= 0) {
+			 subjects.remove(index);
+			 System.out.println("the subject" + subjectId + "is deleted");
+		 }
+		 else {
 			 System.out.println("the subject has not been registered");
 			 return;
 		 }
-		 if (subject.id == subjectId) {
-			 subject = null;
-			 System.out.println("the subject is deleted");
-		 }
+
 	}
+
 	
 	public void editSubject() {
 		 System.out.print("Subject ID: ");
 		 int subjectId = input.nextInt();
-		 if (subject.id == subjectId) {
-			 int num = -1;	  
-			 while(num != 5) {
-				 System.out.println("*** Subject Info Edit Menu ***");
-				 System.out.println(" 1. Edit Id");
-				 System.out.println(" 2. Edit Name");
-				 System.out.println(" 3. Edit Time");
-				 System.out.println(" 4. Edit Professor");
-				 System.out.println(" 5. Exit");
-				 System.out.println("Select one number between 1 - 5:");
-				 num = input.nextInt();
-				 if (num == 1) {
-					 System.out.print("Subject ID: ");
-					 subject.id= input.nextInt();
+		 for (int i = 0; i<subjects.size(); i++) {
+			 Subject subject = subjects.get(i);
+			 if (subject.id == subjectId) {
+				 int num = -1;	  
+				 while(num != 5) {
+					 System.out.println("*** Subject Info Edit Menu ***");
+					 System.out.println(" 1. Edit Id");
+					 System.out.println(" 2. Edit Name");
+					 System.out.println(" 3. Edit Time");
+					 System.out.println(" 4. Edit Professor");
+					 System.out.println(" 5. Exit");
+					 System.out.println("Select one number between 1 - 5:");
+					 num = input.nextInt();
+					 if (num == 1) {
+						 System.out.print("Subject ID: ");
+						 subject.id= input.nextInt();
+					 }
+					 else if (num == 2) {
+						 System.out.print("Subject name: ");
+						 subject.name = input.next();			
+					 }
+					 else if (num == 3) {
+						 System.out.println("Subject time");
+						 System.out.print("Day:");
+						 subject.day = input.next();		
+						 System.out.print("Start Time: ");
+						 subject.starttime = input.nextInt();
+						 System.out.print("Finish Time: ");
+						 subject.finishtime = input.nextInt();		
+					 }
+					 else if (num == 4) {
+						 System.out.print("Professor: ");
+						 subject.professor = input.next();			
+					 }
+					 else {
+						 continue;
+					 }
 				 }
-				 else if (num == 2) {
-					 System.out.print("Subject name: ");
-					 subject.name = input.next();			
-				 }
-				 else if (num == 3) {
-					 System.out.println("Subject time");
-					 System.out.print("Day:");
-					 subject.day = input.next();		
-					 System.out.print("Start Time: ");
-					 subject.starttime = input.nextInt();
-					 System.out.print("Finish Time: ");
-					 subject.finishtime = input.nextInt();		
-				 }
-				 else if (num == 4) {
-					 System.out.print("Professor: ");
-					 subject.professor = input.next();			
-				 }
-				 else {
-					 continue;
-				 }
-			}
-			 
+				 break;
+			 } 
 		 }
 	}
 	
-	public void viewSubject() {;
-		 System.out.print("Subject ID: ");
-		 int subjectId = input.nextInt();
-		 if (subject.id == subjectId) {
-			 subject.printInfo();
+	public void viewSubjects() {
+//		 System.out.print("Subject ID: ");
+//		 int subjectId = input.nextInt();
+		 for (int i = 0; i<subjects.size(); i++) {
+			 subjects.get(i).printInfo();
 		 }
-	}
+	}	 
 }
+
