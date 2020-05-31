a1a091153d1283dda4914ce14a970cfade4f9b68
최석준 <@DESKTOP-LVBSNLU>
2020-05-31 18:35:45 +0900

    Gui

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 import log.EventLogger;
 
